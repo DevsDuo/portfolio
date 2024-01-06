499159583659a86fcf856600b1ffac76f6911a82
arthurbcg <arthurbcg16@gmail.com>
2024-01-06 16:26:53 -0300

    crate-header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import './App.css'
-import { FaBeer } from "react-icons/fa";
-
-
+import Header from "./components/Header"
 
 function App() {
   return (
     <>
-      <div>
-        <span>Hello World</span> <br/>
-        <span>Sejam bem vindos, somos a Devs Duo</span>
-        
-        <FaBeer />
-      </div>
+      <Header />
     </>
   )
 }

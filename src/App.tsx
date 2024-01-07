a01c2828c75301f8a49a6ef95e6e1b7893094d8c
arthurbcg <arthurbcg16@gmail.com>
2024-01-07 01:50:42 -0300

    finished-page-home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from "./components/Header"
 import { AboutUs } from './pages/about_us'
+import Home from './pages/home'
 
 function App() {
   return (
     <>
       <Header />
+      <Home />
       <AboutUs />
     </>
   )
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import * as S from "./styles"
+
+import LogoHome from "../../assets/logo-principal.png"
+
+const Home: React.FC = () => {
+  return(
+    <S.ContainerHome id='home'>
+        <S.LogoHome src={LogoHome} />
+        <S.TextsHome>
+            <h1>Hello World</h1>
+            <span>Sejam bem vindos, somos a <S.DevsDuoHome>Devs Duo</S.DevsDuoHome>!</span>
+        </S.TextsHome>
+    </S.ContainerHome>
+  );
+}
+
+export default Home;

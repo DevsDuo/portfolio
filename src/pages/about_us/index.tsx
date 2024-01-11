a4217f10d0d1723111543bf2c862a34729a0234d
arthurbcg <arthurbcg16@gmail.com>
2024-01-11 15:57:06 -0300

    text-about-us-correct

--- src/pages/about_us/index.tsx
+++ src/pages/about_us/index.tsx
@@ -7,8 +7,10 @@ export function AboutUs() {
             <S.AboutVertical>
                 <S.PageTitle>SOBRE NÓS</S.PageTitle>
                 <S.AboutHorizontal>
-                    <div>Esse Texto Tava na fonte INTER, mas n sei oq era pra fazer entao nao mudei pra poppins, deixei assim <br /> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s.
-                    <br /><br />Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    <div>
+                        Em um universo digital em constante expansão, a Devs Duo emerge como um farol de inovação e autonomia no desenvolvimento de sistemas. Esta empresa singular é moldada pelas mentes criativas e empreendedoras de Arthur e Guilherme, uma dupla dinâmica decidida a deixar sua marca no cenário tecnológico.
+                        <br/> <br/> Arthur e Guilherme, fundadores da Devs Duo, personificam a essência da colaboração. Combinando suas habilidades complementares e visões alinhadas, eles criaram uma empresa que não apenas desenvolve sistemas, mas também cultiva uma abordagem única e personalizada para cada projeto.
+                        <br/> <br/> O nome "Devs Duo" não apenas encapsula a natureza colaborativa da empresa, mas também representa a expertise dual de Arthur e Guilherme. Juntos, eles formam uma equipe coesa capaz de enfrentar desafios complexos e entregar resultados de alta qualidade.
                     </div>
                     <img src={logo}/>
                 </S.AboutHorizontal>
